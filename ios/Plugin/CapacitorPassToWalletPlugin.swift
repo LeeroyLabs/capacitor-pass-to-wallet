e00862fe84f2e335c9352cd12930731be0947cbc
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2025-05-14 13:24:42 -0400

    Change reading technique
    Drop the base64, does not work properly when base64 is a bigger than a certain size. With this change, we pass the URL of the pass instead of base64. To not change many things, you still pass it as {base64: url}. The reason behind this is because capacitor / webkit is pretty bad at downloading files that are passed a certain size and it truncates the data, but swift does not have this limitation.

--- ios/Plugin/CapacitorPassToWalletPlugin.swift
+++ ios/Plugin/CapacitorPassToWalletPlugin.swift
@@ -2,61 +2,51 @@ import Foundation
 import Capacitor
 import PassKit
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(CapacitorPassToWalletPlugin)
 public class CapacitorPassToWalletPlugin: CAPPlugin {
     private let implementation = CapacitorPassToWallet()
-
+    
     @objc func addToWallet(_ call: CAPPluginCall) {
-        let data = call.getString("base64") ?? ""
-
-        if let dataPass = Data(base64Encoded: data, options: .ignoreUnknownCharacters) {
-            if let pass = try? PKPass(data: dataPass) {
-                if PKPassLibrary().containsPass(pass) {
+        let url = call.getString("base64") ?? ""
+        let passURL = URL(string: url)!;
+        
+        if let data = try? Data(contentsOf: passURL, options: .mappedIfSafe) {
+            if let pkpass = try? PKPass(data: data) {
+                if PKPassLibrary().containsPass(pkpass) {
                     let error =
                         """
                     {"code": 100,"message": "Pass already added"}
-                    """
-                    call.reject(error)
-                } else {
-                    if let vc = PKAddPassesViewController(pass: pass) {
-                        call.resolve([
-                            "value": implementation.echo("SUCCESS")
-                        ])
-
-                        DispatchQueue.main.async {
-                            self.bridge?.viewController?.present(vc, animated: true, completion: nil)
-                        }
+                    """;
+                    call.reject(error);
+                } else if let vc = PKAddPassesViewController(pass: pkpass) {
+                    call.resolve(["value": implementation.echo("SUCCESS")]);
+                    
+                    DispatchQueue.main.async {
+                        self.bridge?.viewController?.present(vc, animated: true, completion: nil);
                     }
                 }
             } else {
                 let error =
                     """
                 {"code": 101,"message": "PKPASS file has invalid data"}
-                """
-
-                call.reject(error)
+                """;
+                call.reject(error);
             }
         } else {
             let error =
-                """
-            {"code": 102,"message": "Error with base64 data"}
-            """
-            call.reject(error)
-
+                    """
+                {"code": 101,"message": "PKPASS file has invalid data"}
+                """;
+            call.reject(error);
         }
-
     }
-
+    
     @objc func addMultipleToWallet(_ call: CAPPluginCall) {
         let data = call.getArray("base64") ?? []
-
+        
         var pkPasses = [PKPass]()
         var duplicatedAmount = 0
-
+        
         for base64 in data {
             if let dataPass = Data(base64Encoded: base64 as! String, options: .ignoreUnknownCharacters) {
                 if let pass = try? PKPass(data: dataPass) {
@@ -68,29 +58,25 @@ public class CapacitorPassToWalletPlugin: CAPPlugin {
                 }
             }
         }
-
+        
         if pkPasses.count > 0 {
             if let vc = PKAddPassesViewController(passes: pkPasses) {
                 call.resolve([
                     "value": implementation.echo("SUCCESS")
                 ])
-
-                DispatchQueue.main.async {
-                    self.bridge?.viewController?.present(vc, animated: true, completion: nil)
-                }
+                self.bridge?.viewController?.present(vc, animated: true, completion: nil)
             }
         } else {
             let error = duplicatedAmount == 0
-                ?
+            ?
                 """
             {"code": 103,"message": "PKPASSES file has invalid data"}
             """
-                :
+            :
                 """
             {"code": 100,"message": "Passes already added"}
             """
             call.reject(error)
         }
-
     }
 }
